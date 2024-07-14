8165a2ec02f15d1a40dd8dcca85e1a479be779b4
102092 <quaint3@gmail.com>
2024-07-14 17:29:06 +0900

    Title: 힙 정렬 구현, Time: 735ms, Memory: 32MB, Status: Passed - CodeTree

--- 240714/힙 정렬 구현/implement-heap-sort.py
+++ 240714/힙 정렬 구현/implement-heap-sort.py
@@ -16,11 +16,9 @@ def heapify(arr, n, i):
 def heap_sort(arr):
     n = len(arr)
     
-    # Build max heap
     for i in range(n // 2 - 1, -1, -1):
         heapify(arr, n, i)
     
-    # Extract elements from heap one by one
     for i in range(n - 1, 0, -1):
         arr[0], arr[i] = arr[i], arr[0]
         heapify(arr, i, 0)
